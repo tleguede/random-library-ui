922cccb9b81a0ca75b8c319551adc2a11ccf55a7
Legrand <tleguede@gmail.com>
2021-07-26 14:43:07 +0000

    some update

--- src/redux/apis/library.ts
+++ src/redux/apis/library.ts
@@ -1,7 +1,11 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 import {Author, Book, EditionDate, LiteraryMovement} from "../types";
-import {BASE_SERVER_URI} from "../constants";
-import {useMemo} from "react";
+import {
+    BASE_SERVER_URI,
+    BOOK_GET_ALL_AUTHORS, BOOK_GET_ALL_BOOKS,
+    BOOK_GET_ALL_EDITION_DATES,
+    BOOK_GET_ALL_LITERARY_MOVEMENTS
+} from "../constants";
 
 // Define a service using a base URL and expected endpoints
 export const libraryApi = createApi({
@@ -9,20 +13,19 @@ export const libraryApi = createApi({
     baseQuery: fetchBaseQuery({baseUrl: BASE_SERVER_URI}),
     endpoints: (builder) => ({
         allAuthors: builder.query<Author[], string>({
-            query: (name) => `authors/`,
+            query: (name) => BOOK_GET_ALL_AUTHORS,
         }),
         allEditionDates: builder.query<EditionDate[], string>({
-            query: (name) => `edition_dates/`,
+            query: (name) => BOOK_GET_ALL_EDITION_DATES,
         }),
         allLiteraryMovements: builder.query<LiteraryMovement[], string>({
-            query: (name) => `literary_movements/`,
+            query: (name) => BOOK_GET_ALL_LITERARY_MOVEMENTS,
         }),
         allBooks: builder.query<Book[], string>({
-            query: (name) => `books/`,
+            query: (name) => BOOK_GET_ALL_BOOKS,
         }),
     }),
 })
-
 
 
 // Export hooks for usage in functional components, which are
--- src/redux/constants.ts
+++ src/redux/constants.ts
@@ -2,6 +2,8 @@ export const BASE_SERVER_URI = "http://localhost:4000";
 
 export const BOOK_CREATION_URI = `${BASE_SERVER_URI}/create`;
 
+export const BOOK_GET_ALL_BOOKS=`${BASE_SERVER_URI}/books`;
+
 export const BOOK_GET_ALL_AUTHORS=`${BASE_SERVER_URI}/authors`;
 
 export const BOOK_GET_ALL_EDITION_DATES=`${BASE_SERVER_URI}/edition_dates`;
